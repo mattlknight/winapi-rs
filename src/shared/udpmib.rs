2566977ab7379a546c9a573aefc7eddb0cb3c855
Matthew Knight <mattlknight@gmail.com>
2018-09-02 16:13:07 -0500

    More code cleanup

--- src/shared/udpmib.rs
+++ src/shared/udpmib.rs
@@ -4,48 +4,37 @@
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your option.
 // All files in the project carrying such notice may not be copied, modified, or distributed
 // except according to those terms.
-
+//! Contains the public definitions and structures for the UDP-specific parts of MIB-II
 // #include <winapifamily.h>
-
-// use shared::guiddef::GUID;
-// use shared::ws2def::*;
-// use um::minwinbase::{LPOVERLAPPED,OVERLAPPED};
-// use ctypes::{INT};
 use shared::basetsd::DWORD64;
 use shared::in6addr::IN6_ADDR;
 use shared::minwindef::DWORD;
 use shared::ntdef::{INT, LARGE_INTEGER, UCHAR, ULONGLONG};
-
-pub const TCPIP_OWNING_MODULE_SIZE: usize = 16;
-
 const ANY_SIZE: usize = 1;
-
-
+pub const TCPIP_OWNING_MODULE_SIZE: usize = 16;
 STRUCT!{struct MIB_UDPROW {
     dwLocalAddr: DWORD,
     dwLocalPort: DWORD,
 }}
 pub type PMIB_UDPROW = *mut MIB_UDPROW;
-
 STRUCT!{struct MIB_UDPTABLE {
     dwNumEntries: DWORD,
     table: [MIB_UDPROW; ANY_SIZE],
 }}
 pub type PMIB_UDPTABLE = *mut MIB_UDPTABLE;
-
+// FIXME: Need to figure out how to define SIZEOF_UDPTABLE(X)
 STRUCT!{struct MIB_UDPROW_OWNER_PID {
     dwLocalAddr: DWORD,
     dwLocalPort: DWORD,
     dwOwningPid: DWORD,
 }}
 pub type PMIB_UDPROW_OWNER_PID = *mut MIB_UDPROW_OWNER_PID;
-
 STRUCT!{struct MIB_UDPTABLE_OWNER_PID {
     dwNumEntries: DWORD,
     table: [MIB_UDPROW_OWNER_PID; ANY_SIZE],
 }}
 pub type PMIB_UDPTABLE_OWNER_PID = *mut MIB_UDPTABLE_OWNER_PID;
-
+// FIXME: Need to figure out how to define SIZEOF_UDPTABLE_OWNER_PID(X)
 STRUCT!{struct MIB_UDPROW_OWNER_MODULE_u_s {
     bitfield: INT,
 }}
@@ -57,7 +46,6 @@ UNION!{union MIB_UDPROW_OWNER_MODULE_u {
     s s_mut: MIB_UDPROW_OWNER_MODULE_u_s,
     dwFlags dwFlags_mut: INT,
 }}
-
 STRUCT!{struct MIB_UDPROW_OWNER_MODULE {
     dwLocalAddr: DWORD,
     dwLocalPort: DWORD,
@@ -67,26 +55,24 @@ STRUCT!{struct MIB_UDPROW_OWNER_MODULE {
     OwningModuleInfo: [ULONGLONG; TCPIP_OWNING_MODULE_SIZE],
 }}
 pub type PMIB_UDPROW_OWNER_MODULE = *mut MIB_UDPROW_OWNER_MODULE;
-
 STRUCT!{struct MIB_UDPTABLE_OWNER_MODULE {
     dwNumEntries: DWORD,
     table: [MIB_UDPROW_OWNER_MODULE; ANY_SIZE],
 }}
 pub type PMIB_UDPTABLE_OWNER_MODULE = *mut MIB_UDPTABLE_OWNER_MODULE;
-
+// FIXME: Need to figure out how to define SIZEOF_UDPTABLE_OWNER_MODULE(X)
 STRUCT!{struct MIB_UDP6ROW {
     dwLocalAddr: IN6_ADDR,
     dwLocalScopeId: DWORD,
     dwLocalPort: DWORD,
 }}
 pub type PMIB_UDP6ROW = *mut MIB_UDP6ROW;
-
 STRUCT!{struct MIB_UDP6TABLE {
     dwNumEntries: DWORD,
     table: [MIB_UDP6ROW; ANY_SIZE],
 }}
 pub type PMIB_UDP6TABLE = *mut MIB_UDP6TABLE;
-
+// FIXME: Need to figure out how to define SIZEOF_UDP6TABLE(X)
 STRUCT!{struct MIB_UDP6ROW_OWNER_PID {
     ucLocalAddr: [UCHAR; 16],
     dwLocalScopeId: DWORD,
@@ -94,14 +80,13 @@ STRUCT!{struct MIB_UDP6ROW_OWNER_PID {
     dwOwningPid: DWORD,
 }}
 pub type PMIB_UDP6ROW_OWNER_PID = *mut MIB_UDP6ROW_OWNER_PID;
-
 STRUCT!{struct MIB_UDP6TABLE_OWNER_PID {
     dwNumEntries: DWORD,
-    // _Field_size_(dwNumEntries)
+    // _Field_size_(dwNumEntries) // FIXME: Unsure what this is/means
     table: [MIB_UDP6ROW_OWNER_PID; ANY_SIZE],
 }}
 pub type PMIB_UDP6TABLE_OWNER_PID = *mut MIB_UDP6TABLE_OWNER_PID;
-
+// FIXME: Need to figure out how to define SIZEOF_UDP6TABLE_OWNER_PID(X)
 STRUCT!{struct MIB_UDP6ROW_OWNER_MODULE_u_s {
     bitfield: INT,
 }}
@@ -123,13 +108,12 @@ STRUCT!{struct MIB_UDP6ROW_OWNER_MODULE {
     OwningModuleInfo: [ULONGLONG; TCPIP_OWNING_MODULE_SIZE],
 }}
 pub type PMIB_UDP6ROW_OWNER_MODULE = *mut MIB_UDP6ROW_OWNER_MODULE;
-
 STRUCT!{struct MIB_UDP6TABLE_OWNER_MODULE {
     dwNumEntries: DWORD,
     table: [MIB_UDP6ROW_OWNER_MODULE; ANY_SIZE],
 }}
 pub type PMIB_UDP6TABLE_OWNER_MODULE = *mut MIB_UDP6TABLE_OWNER_MODULE;
-
+// FIXME: Need to figure out how to define SIZEOF_UDP6TABLE_OWNER_MODULE(X)
 STRUCT!{struct MIB_UDPSTATS {
     dwInDatagrams: DWORD,
     dwNoPorts: DWORD,
@@ -138,8 +122,6 @@ STRUCT!{struct MIB_UDPSTATS {
     dwNumAddrs: DWORD,
 }}
 pub type PMIB_UDPSTATS = *mut MIB_UDPSTATS;
-
-// if (NTDDI_VERSION >= NTDDI_WIN10_RS3)
 STRUCT!{struct MIB_UDPSTATS2 {
     dw64InDatagrams: DWORD64,
     dwNoPorts: DWORD,
