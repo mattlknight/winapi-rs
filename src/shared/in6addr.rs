2832ada0119d16efe23c24318d361b7419ad59b4
Matthew Knight <mattlknight@gmail.com>
2018-09-02 18:42:24 -0500

    Fix typo in Module Doc Comment

--- src/shared/in6addr.rs
+++ src/shared/in6addr.rs
@@ -4,7 +4,7 @@
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your option.
 // All files in the project carrying such notice may not be copied, modified, or distributed
 // except according to those terms.
-//! IPv4 Internet address, 'on-wire' format structure.
+//! IPv6 Internet address, 'on-wire' format structure.
 use shared::minwindef::{BYTE, WORD};
 UNION!{union in6_addr_u {
     [u16; 8],
